0dffdf2faab9e777b4485de62d45a7d46cc79dcc
Andrew <andrew.romanof@ontariotechu.net>
2020-03-05 19:46:31 -0500

    assignment

--- assignmentq4/src/sample/Main.java
+++ assignmentq4/src/sample/Main.java
@@ -13,24 +13,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     Pane pane = new Pane();
-    TextField textField = new TextField();
+    TextField text = new TextField();
     VBox box = new VBox();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Label lblFilename = new Label("Filename:", textField);
-        lblFilename.setContentDisplay(ContentDisplay.RIGHT);
-        textField.setPrefColumnCount(20);
+        Label fileName = new Label("Filename:", text);
+        fileName.setContentDisplay(ContentDisplay.RIGHT);
         Button btView = new Button("View");
 
-        HBox hBox = new HBox(lblFilename, btView);
+        HBox hBox = new HBox(fileName, btView);
 
         box.getChildren().addAll(pane, hBox);
         Scene scene = new Scene(box);
         primaryStage.setScene(scene);
         btView.setOnAction(e-> {
             createHistogram();
-            box.setTranslateY(10);
             primaryStage.sizeToScene();
 
         });
